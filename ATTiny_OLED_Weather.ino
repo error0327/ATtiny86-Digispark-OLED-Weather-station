e25a8c00fe0655e7b0bec1d2cb1fde279b0820ba
Obada abu alhiga <error0327@gmail.com>
2018-01-22 11:00:17 +0200

    Add files via upload

---
+++ ATTiny_OLED_Weather.ino
@@ -0,0 +1,42 @@
+
+#include "SSD1306_minimal.h"
+#include <dht.h>
+#include <avr/pgmspace.h>
+
+SSD1306_Mini oled;
+dht DHT;
+
+// Pins Used :  for the DHT11 i am using PIN 3 , and for the screen SDA on PIN 0 and forh SL i use PIN 2
+#define DHT11_PIN 3
+
+
+void PrintData() {
+ oled.startScreen();
+ oled.clear(); // Clears the display
+
+ int chk = DHT.read11(DHT11_PIN);
+
+ oled.cursorTo(0, 0); // x:0, y:0
+ oled.printString("HomeWeatherStation");
+ oled.cursorTo(0, 15); // x:0, y:23
+ oled.printStringInt("Temperature : ",DHT.temperature);
+ oled.cursorTo(0, 20); // x:0, y:23
+ oled.printStringInt("Humidity : ",DHT.humidity);
+ delay(5000);
+ 
+}
+
+
+void setup()
+{
+  Serial.begin(9600); // we need this for the DHT11
+  oled.init(0x3c);  //oled address .
+  
+  oled.clear();
+  delay(1000);
+}
+ 
+void loop()
+{
+ PrintData();
+} 
